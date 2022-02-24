a366f65a3814ded010d0aac377568b9915e3d369
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-02-24 15:11:15 +0300

    preloader window onload

--- preloader.js
+++ preloader.js
@@ -1,9 +1,21 @@
 // анимация плавного скрытия прелоадера
 
-document.addEventListener('DOMContentLoaded', function () {
-	document.body.classList.add('loaded_hiding');
-	window.setTimeout(function () {
-	  document.body.classList.add('loaded');
-	  document.body.classList.remove('loaded_hiding');
-	}, 500);
-});
+// document.addEventListener('DOMContentLoaded', function () {
+// 	document.body.classList.add('loaded_hiding');
+// 	window.setTimeout(function () {
+// 	  document.body.classList.add('loaded');
+// 	  document.body.classList.remove('loaded_hiding');
+// 	}, 500);
+// });
+
+
+// этот код на мобильных вызывал неприятный баг
+// страница отображалась только через полторы минуты
+// из-за медленной загрузки картинок
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+    	document.body.classList.add('loaded');
+    	document.body.classList.remove('loaded_hiding');
+    }, 300);
+}
